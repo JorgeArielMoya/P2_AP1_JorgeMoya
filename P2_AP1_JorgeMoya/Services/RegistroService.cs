6ab4f1264fb9c9fc351d87cea24784e20d07bc99
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-27 19:42:39 -0400

    Metodo listar agregado

--- P2_AP1_JorgeMoya/Services/RegistroService.cs
+++ P2_AP1_JorgeMoya/Services/RegistroService.cs
@@ -1,5 +1,19 @@
-﻿namespace P2_AP1_JorgeMoya.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using P2_AP1_JorgeMoya.DAL;
+using P2_AP1_JorgeMoya.Models;
+using System.Linq.Expressions;
 
-public class RegistroService
+namespace P2_AP1_JorgeMoya.Services;
+
+public class RegistroService (IDbContextFactory<Contexto> Dbfactory)
 {
+    public async Task<List<Registros>> Listar (Expression<Func<Registros, bool>> criterio)
+    {
+        await using var contexto = await Dbfactory.CreateDbContextAsync();
+        return await contexto.Registros
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+
+    }
 }
