27e7970aba37b8a7d3eedcccad2004620983911c
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-27 18:47:34 -0400

    Contexto con DbSet generico creado

--- P2_AP1_JorgeMoya/DAL/Contexto.cs
+++ P2_AP1_JorgeMoya/DAL/Contexto.cs
@@ -1,5 +1,10 @@
-﻿namespace P2_AP1_JorgeMoya.DAL;
+﻿using Microsoft.EntityFrameworkCore;
+using P2_AP1_JorgeMoya.Models;
 
-public class Contexto
+namespace P2_AP1_JorgeMoya.DAL;
+
+public class Contexto : DbContext
 {
+    public DbSet<Registros> Registros { get; set; }
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 }
