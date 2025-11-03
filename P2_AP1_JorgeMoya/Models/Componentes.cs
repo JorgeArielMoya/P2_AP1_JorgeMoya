91b0b905a884d544b52214a40601e8aefbdb7928
jorge <jorge@LAPTOP-D8GDLQRL>
2025-11-03 18:42:21 -0400

    Modelos creados

---
+++ P2_AP1_JorgeMoya/Models/Componentes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_AP1_JorgeMoya.Models;
+
+public class Componentes
+{
+    [Key]
+    public int ComponenteId { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    public decimal Precio { get; set; }
+    public int Existencia { get; set; }
+}
---
+++ P2_AP1_JorgeMoya/Models/PedidosDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P2_AP1_JorgeMoya.Models;
+
+public class PedidosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int ComponenteId { get; set; }
+    public int PedidoId { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Precio { get; set; }
+
+    [ForeignKey(nameof(PedidoId))]
+    public virtual Pedidos Pedido { get; set; }
+
+    [ForeignKey(nameof(ComponenteId))]
+    public virtual Pedidos Componente { get; set; }
+}
