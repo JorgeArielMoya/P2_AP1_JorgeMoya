90aba0da4996047edb04a747b498057532dcdd0f
jorge <jorge@LAPTOP-D8GDLQRL>
2025-11-03 21:03:35 -0400

    cambios

--- P2_AP1_JorgeMoya/DAL/Contexto.cs
+++ P2_AP1_JorgeMoya/DAL/Contexto.cs
@@ -9,68 +9,68 @@ public class Contexto : DbContext
     public DbSet<Componentes> Componentes { get; set; }
     public DbSet<PedidosDetalle> PedidosDetalles { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Componentes>().HasData(
-            new List<Componentes>()
-            {
-                new ()
-                {
-                    ComponenteId = 1,
-                    Descripcion = "Memoria 4GB",
-                    Precio = 1580,
-                    Existencia = 1
-                },
-
-                new ()
-                {
-                    ComponenteId = 2,
-                    Descripcion = "Disco SSD 120MB",
-                    Precio = 4200,
-                    Existencia = 8
-                },
-
-                new ()
-                {
-                    ComponenteId = 3,
-                    Descripcion = "Tarjeta de Video",
-                    Precio = 10000,
-                    Existencia = 4
-                }
-            });
-        base.OnModelCreating(modelBuilder);
-    }
-
     //protected override void OnModelCreating(ModelBuilder modelBuilder)
     //{
-    //    base.OnModelCreating(modelBuilder);
-
-    //    modelBuilder.Entity<Componentes>(entity =>
-    //    {
-    //        entity.HasData(
-    //            new Componentes
+    //    modelBuilder.Entity<Componentes>().HasData(
+    //        new List<Componentes>()
+    //        {
+    //            new ()
     //            {
     //                ComponenteId = 1,
     //                Descripcion = "Memoria 4GB",
     //                Precio = 1580,
     //                Existencia = 1
     //            },
-    //            new Componentes
+
+    //            new ()
     //            {
     //                ComponenteId = 2,
     //                Descripcion = "Disco SSD 120MB",
     //                Precio = 4200,
     //                Existencia = 8
     //            },
-    //            new Componentes
+
+    //            new ()
     //            {
     //                ComponenteId = 3,
     //                Descripcion = "Tarjeta de Video",
     //                Precio = 10000,
     //                Existencia = 4
     //            }
-    //        );
-    //    });
+    //        });
+    //    base.OnModelCreating(modelBuilder);
     //}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Componentes>(entity =>
+        {
+            entity.HasData(
+                new Componentes
+                {
+                    ComponenteId = 1,
+                    Descripcion = "Memoria 4GB",
+                    Precio = 1580,
+                    Existencia = 1
+                },
+                new Componentes
+                {
+                    ComponenteId = 2,
+                    Descripcion = "Disco SSD 120MB",
+                    Precio = 4200,
+                    Existencia = 8
+                },
+                new Componentes
+                {
+                    ComponenteId = 3,
+                    Descripcion = "Tarjeta de Video",
+                    Precio = 10000,
+                    Existencia = 4
+                }
+            );
+        });
+    }
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 }
